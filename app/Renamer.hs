f9c440e61ee9bf0bc5c729e5380248e1c3bdee4f
Waulk <jap123al@gmail.com>
2025-11-15 14:29:58 -0800

    Updated renamer
    Added some comments, added an error as discussed previously, and cleaned up some code slightly.

--- app/Renamer.hs
+++ app/Renamer.hs
@@ -1,11 +1,11 @@
 module Renamer (renameFormula) where
--- Intended to rename variables (a first step to turning into clausal form)
--- Does not handle an error of putting quantifiers inside of other quantifiers at the moment
--- Free variables get to keep their name as is
 import Datatypes
 import Data.Map (Map)
 import qualified Data.Map as Map
 
+-- Does the actual renaming of variables in terms
+-- If the term is a function then it recursively renames into the term
+-- Parameters: Mapping, Input term
 rename :: Map String (Int, Bool) -> Term -> Term
 rename dict (Variable name) = 
     case Map.lookup name dict of
@@ -13,9 +13,11 @@ rename dict (Variable name) =
         _ -> Variable name
 rename dict (Function name terms ) = 
     Function name (map (rename dict) terms)
-rename dict other =
+rename _ other =
     other
 
+-- Handles renaming binary formulae (such as And)
+-- Parameters: Binary operation type, left formula, right formula, mapping
 renameBinary :: (Formula -> Formula -> Formula) -> Formula -> Formula -> Map String (Int, Bool) -> (Map String (Int, Bool), Formula)
 renameBinary t a b dict =
   let
@@ -24,6 +26,25 @@ renameBinary t a b dict =
   in
     (dict2, t newA newB) 
 
+-- Handles renaming quantifiers
+-- Renames the quantified variable to (Number of times this variable name has been seen in quantifiers)_(Variable name)
+-- Keeps track of if the variable is quantified, and if it has already been quantified before the call it errors
+-- Parameters: Quantifier type, name of variable quantified, formula, mapping
+renameQuantifier :: (String -> Formula -> Formula) -> String -> Formula -> Map String (Int, Bool) -> (Map String (Int, Bool), Formula)
+renameQuantifier t name formula dict =
+    let
+        (count, _) = Map.findWithDefault (0, False) name dict
+        newCount = count + 1
+        newName = (show newCount) ++ "_" ++ name
+        newDict = Map.insert name (newCount, True) dict
+        (_, newF) = renameVariables formula newDict
+    in
+        case Map.lookup name dict of
+          Just (_, True) -> error ("Variable " ++ name ++ " quantified twice in the same scope!")
+          _ -> (Map.insert name (newCount, False) newDict, t newName newF)
+
+-- The main renaming function for this file
+-- Parameters: Formula to be renamed, Mapping (Name -> (Times seen, isQuantified))
 renameVariables :: Formula -> Map String (Int, Bool)-> (Map String (Int, Bool), Formula)
 renameVariables (Predicate name terms) dict = (dict, Predicate name (map (rename dict) terms))
 
@@ -46,25 +67,12 @@ renameVariables (Iff p q) dict =
   renameBinary Iff p q dict
 
 
-renameVariables (ForAll name f) dict =
-    let
-        (count, _) = Map.findWithDefault (0, False) name dict
-        newCount = count + 1
-        newName = (show newCount) ++ "_" ++ name
-        newDict = Map.insert name (newCount, True) dict
-        (_, newF) = renameVariables f newDict
-    in
-        (Map.insert name (newCount, False) newDict, ForAll newName newF)
-renameVariables (ThereExists name f) dict =
-    let
-        (count, _) = Map.findWithDefault (0, False) name dict
-        newCount = count + 1
-        newName = (show newCount) ++ "_" ++ name
-        newDict = Map.insert name (newCount, True) dict
-        (_, newF) = renameVariables f newDict
-    in
-        (Map.insert name (newCount, False) newDict, ThereExists newName newF)
+renameVariables (ForAll name f) dict = renameQuantifier ForAll name f dict
+renameVariables (ThereExists name f) dict = renameQuantifier ThereExists name f dict
+
+-- Swaps the input because the return type is a tuple of the inputs reversed pretty much
 renameVariables x y = (y, x)
 
+-- The actual function that should be called for Step 2b
 renameFormula :: Formula -> Formula
 renameFormula f = snd (renameVariables f Map.empty)
