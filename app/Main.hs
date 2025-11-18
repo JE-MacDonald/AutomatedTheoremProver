3f1f752304b109da0fa5517fb247f6e2ffce2f7b
Ethan MacDonald <jem21@sfu.ca>
2025-11-18 01:32:38 -0800

    changes in main to test each module

--- app/Main.hs
+++ app/Main.hs
@@ -5,20 +5,43 @@ import Parser
 import Renamer
 import ClausalFormConverter
 import qualified Text.Megaparsec as MP
+import FreeVariables (freeVariables)
+import NegationNormalForm (nnfConverter)
+import MoveQuantifiers (moveQuantifiers)
+import Skolemize (skolemize)
 
 main :: IO ()
 main = do
-    --go thru the steps of FO resolution and then refutation by calling
-    --funcs from modules (ie, PNF conversion module, skolem module, etc)
-
-    --test for ClausalFormConverter
-    input <- readFile "input2.txt"
+    --First-order resolution
+    input <- readFile "input3.txt"
     case inputStringToFormula input of
         Left e -> putStrLn(MP.errorBundlePretty e)
         Right formula -> do 
-            let q = renameFormula formula
-            let x = convertToClausalForm q
-            print (show q)
-            putStrLn ""
-            putStrLn (unlines (map show x))
+            let negatedFormula = Not formula
+            putStrLn "Negated Input Formula:"
+            print negatedFormula
+
+            let noFreeVars = freeVariables negatedFormula
+            putStrLn "\nStep 1: Remove Free Variables:"
+            print noFreeVars
+
+            let negNormForm = nnfConverter noFreeVars
+            putStrLn "\nStep 2a: Negation Normal Form:"
+            print negNormForm
+
+            let renamed = renameFormula negNormForm
+            putStrLn "\nStep 2b: Rename Quantified Variables:"
+            print renamed
+
+            let pnfFormula = moveQuantifiers renamed
+            putStrLn "\nStep 2c: Move Quantifiers (PNF):"
+            print pnfFormula
+
+            let skolemized = skolemize pnfFormula
+            putStrLn "\nStep 3: Skolemization:"
+            print skolemized
+
+            let clausalFormula = convertToClausalForm skolemized
+            putStrLn "\nSteps 4&5: CNF and Clausal Form:"
+            putStrLn (unlines (map show clausalFormula))
 
